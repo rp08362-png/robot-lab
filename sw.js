cd39f8db45c692d88dfedb76dc7d0cf1418529c4
rp08362-png <rp08362@gmail.com>
2026-08-13 17:48:35 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'robot-lab-shell-v1';
+const CACHE = 'robot-lab-shell-v2';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./config.js','./manifest.webmanifest','./robot-main.jpg','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
